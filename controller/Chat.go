31a088d832406b27977026d28733021bd315d228
isnaenti <isnaenti16@gmail.com>
2024-07-10 13:30:44 +0700

    edit func chat

--- controller/Chat.go
+++ controller/Chat.go
@@ -1,6 +1,5 @@
 package controller
 
-//mengimpor beberapa package yang dibutuhkan untuk program Go, termasuk package dari repository GitHub eksternal.
 import (
 	"encoding/json"
 	"log"
@@ -14,41 +13,32 @@ import (
 )
 
 func Chat(respw http.ResponseWriter, req *http.Request, tokenmodel string) {
-	// Fungsi Chat menangani request HTTP untuk chat dengan AI
-
-	// Definisikan variable chat untuk menyimpan data request
 	var chat models.AIRequest
 
-	// Decode request body ke dalam struct chat
 	err := json.NewDecoder(req.Body).Decode(&chat)
 	if err != nil {
-		// Jika terjadi error saat parsing request body, kembalikan Bad Request error
 		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "error parsing request body "+err.Error())
 		return
 	}
 
-	// Validasi query, jika kosong kembalikan Bad Request error
 	if chat.Query == "" {
 		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "mohon untuk melengkapi data")
 		return
 	}
 
-	// Inisialisasi client untuk request ke Hugging Face API
 	client := resty.New()
 
-	// Ambil Hugging Face API URL dan token dari environment variable
+	// Hugging Face API URL dan token
 	apiUrl := modules.GetEnv("HUGGINGFACE_API_KEY")
 	apiToken := "Bearer " + tokenmodel
 
-	// Variable untuk menyimpan response dan error dari API
 	var response *resty.Response
 	var retryCount int
 	maxRetries := 5
 	retryDelay := 20 * time.Second
 
-	// Looping untuk melakukan request ke Hugging Face API dengan mekanisme retry
+	// Request ke Hugging Face API
 	for retryCount < maxRetries {
-		// Set header untuk Authorization dan Content-Type
 		response, err = client.R().
 			SetHeader("Authorization", apiToken).
 			SetHeader("Content-Type", "application/json").
@@ -56,13 +46,10 @@ func Chat(respw http.ResponseWriter, req *http.Request, tokenmodel string) {
 			Post(apiUrl)
 
 		if err != nil {
-			// Jika terjadi error saat request, log error dan lanjutkan ke retry
 			log.Fatalf("Error making request: %v", err)
 		}
 
-		// Cek status code response
 		if response.StatusCode() == http.StatusOK {
-
 			break
 		} else {
 			var errorResponse map[string]interface{}
