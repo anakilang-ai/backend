13029c058b2464afd34693ab0db68d78809bc30a
isnaenti <isnaenti16@gmail.com>
2024-07-10 14:03:27 +0700

    edit http responwriter

--- utils/http.go
+++ utils/http.go
@@ -1,13 +1,11 @@
 package utils
 
-// memuat paket encoding/json untuk encoding dan decoding JSON, paket log untuk logging, dan paket net/http untuk membuat aplikasi HTTP dalam proyek Go.
 import (
 	"encoding/json"
 	"log"
 	"net/http"
 )
 
-// mengirimkan respons HTTP dengan status kode tertentu dan pesan kesalahan dalam format JSON.
 func ErrorResponse(respw http.ResponseWriter, req *http.Request, statusCode int, err, msg string) {
 	resp := map[string]string{
 		"error":   err,
@@ -16,7 +14,6 @@ func ErrorResponse(respw http.ResponseWriter, req *http.Request, statusCode int,
 	WriteJSON(respw, statusCode, resp)
 }
 
-// mengirimkan respons HTTP dengan status kode tertentu dan konten yang telah dikonversi menjadi JSON.
 func WriteJSON(respw http.ResponseWriter, statusCode int, content any) {
 	respw.Header().Set("Content-Type", "application/json")
 	respw.WriteHeader(statusCode)
