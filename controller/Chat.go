a2183a056050ab7ed23a2f1d827b618777e6bcfc
isnaenti <isnaenti16@gmail.com>
2024-07-10 13:14:56 +0700

    cek code status

--- controller/Chat.go
+++ controller/Chat.go
@@ -80,6 +80,7 @@ func Chat(respw http.ResponseWriter, req *http.Request, tokenmodel string) {
 		}
 	}
 
+	// Cek status code response setelah loop retry
 	if response.StatusCode() != 200 {
 		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error from Hugging Face API "+string(response.Body()))
 		return
