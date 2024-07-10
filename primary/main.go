211fd609fc85bfc7fc9cad1df6e4f8e433fbc07d
isnaenti <isnaenti16@gmail.com>
2024-07-10 12:14:57 +0700

    handlefunc main

--- primary/main.go
+++ primary/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/anakilang-ai/backend/routes"
 )
 
+// mendaftarkan fungsi routes.URL untuk menangani semua request yang masuk ke path "/" (root path).  Kemudian program dijalankan pada port 8080 dan menampilkan pesan bahwa server telah aktif di http://localhost:8080.
 func main() {
 	http.HandleFunc("/", routes.URL)
 	port := ":8080"
