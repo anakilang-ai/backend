ffaaa8ae968924fb460846b44ea1fa26b895df1f
isnaenti <isnaenti16@gmail.com>
2024-07-10 13:42:00 +0700

    edit config

--- modules/config.go
+++ modules/config.go
@@ -1,12 +1,10 @@
 package modules
 
-//mengimpor package os dari standard library Go.
 import (
 	"os"
 	// "github.com/joho/godotenv"
 )
 
-// mengambil nilai dari variabel lingkungan dengan nama envName.
 func GetEnv(envName string) string {
 	// envFile, _ := godotenv.Read("../.env")
 	// return envFile[envName]
