dc7029c48a032300fc0deea424bec7c2176442ef
isnaenti <isnaenti16@gmail.com>
2024-07-10 13:06:54 +0700

    log error

--- controller/Chat.go
+++ controller/Chat.go
@@ -56,6 +56,7 @@ func Chat(respw http.ResponseWriter, req *http.Request, tokenmodel string) {
 			Post(apiUrl)
 
 		if err != nil {
+			// Jika terjadi error saat request, log error dan lanjutkan ke retry
 			log.Fatalf("Error making request: %v", err)
 		}
 
