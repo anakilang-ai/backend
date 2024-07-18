06a5453ecc0a4f979d3e33c3b1841dd76b59b0b4
jxxzy <marjuniatiputri@gmail.com>
2024-07-18 18:32:23 +0700

    add function init

--- controller/Register.go
+++ controller/Register.go
@@ -7,9 +7,9 @@ import (
 	"net/http"
 	"strings"
 
-	"github.com/badoux/checkmail"
 	model "github.com/anakilang-ai/backend/models"
 	"github.com/anakilang-ai/backend/utils"
+	"github.com/badoux/checkmail"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"golang.org/x/crypto/argon2"
--- modules/db.go
+++ modules/db.go
@@ -1,10 +1,12 @@
 package modules
 
 import (
+	"log"
+
 	"github.com/anakilang-ai/backend/utils"
 )
 
-var MongoString string = GetEnv("MONGOSTRING")
+var MongoString string = utils.GetEnv("MONGOSTRING")
 
 var mongoinfo = utils.DBInfo{
 	DBString: MongoString,
@@ -12,3 +14,9 @@ var mongoinfo = utils.DBInfo{
 }
 
 var Mongoconn, ErrorMongoconn = utils.MongoConnect(mongoinfo)
+
+func init() {
+	if ErrorMongoconn != nil {
+		log.Fatalf("Failed to connect to MongoDB: %v", ErrorMongoconn)
+	}
+}
