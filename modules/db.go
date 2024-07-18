a99f21762b25a3b624a4065ee049efdbde6cf3a4
jxxzy <marjuniatiputri@gmail.com>
2024-07-18 18:38:41 +0700

    Menggunakan log.Fatalf untuk mencetak pesan kesalahan

--- modules/db.go
+++ modules/db.go
@@ -1,12 +1,10 @@
 package modules
 
 import (
-	"log"
-
 	"github.com/anakilang-ai/backend/utils"
 )
 
-var MongoString string = utils.GetEnv("MONGOSTRING")
+var MongoString string = GetEnv("MONGOSTRING")
 
 var mongoinfo = utils.DBInfo{
 	DBString: MongoString,
@@ -14,9 +12,3 @@ var mongoinfo = utils.DBInfo{
 }
 
 var Mongoconn, ErrorMongoconn = utils.MongoConnect(mongoinfo)
-
-func init() {
-	if ErrorMongoconn != nil {
-		log.Fatalf("Failed to connect to MongoDB: %v", ErrorMongoconn)
-	}
-}
