0d5c8cedb76963ff80df1d2cea7f8e1b4f620519
Ratusukmakomala <ratukomala28@gmail.com>
2024-07-18 19:02:47 +0700

    edit config membaca file .env

--- modules/config.go
+++ modules/config.go
@@ -1,22 +1,12 @@
 package modules
 
 import (
-	"log"
 	"os"
-
-	"github.com/joho/godotenv"
+	// "github.com/joho/godotenv"
 )
 
-// LoadEnv loads the environment variables from the .env file if it exists
-func LoadEnv() {
-	err := godotenv.Load("../.env")
-	if err != nil {
-		log.Printf("Warning: .env file not found or could not be read, proceeding with environment variables from the OS: %v", err)
-	}
-}
-
-// GetEnv retrieves the value of the environment variable named by the key.
-// It returns the value, which will be empty if the variable is not present.
 func GetEnv(envName string) string {
+	// envFile, _ := godotenv.Read("../.env")
+	// return envFile[envName]
 	return os.Getenv(envName)
 }
