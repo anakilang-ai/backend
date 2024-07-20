9ce6f6c533e29d0df5d8424e4d6e1b57c3f53e05
Ratusukmakomala <ratukomala28@gmail.com>
2024-07-20 19:05:53 +0700

    Validasi Format Email

--- controller/Chat.go
+++ controller/Chat.go
@@ -15,79 +15,56 @@ import (
 	"golang.org/x/crypto/argon2"
 )
 
-// SignUp menangani pendaftaran pengguna baru, termasuk validasi data dan penyimpanan di database
 func SignUp(db *mongo.Database, col string, respw http.ResponseWriter, req *http.Request) {
 	var user model.User
 
-	// Mengurai body permintaan menjadi objek user
 	err := json.NewDecoder(req.Body).Decode(&user)
 	if err != nil {
-		utils.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "Kesalahan dalam parsing body permintaan: "+err.Error())
+		utils.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "error parsing request body "+err.Error())
 		return
 	}
 
-	// Validasi data pengguna
 	if user.NamaLengkap == "" || user.Email == "" || user.Password == "" || user.Confirmpassword == "" {
-		utils.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "Mohon untuk melengkapi data")
+		utils.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "mohon untuk melengkapi data")
 		return
 	}
-
-	// Validasi format email
 	if err := checkmail.ValidateFormat(user.Email); err != nil {
-		utils.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "Email tidak valid")
+		utils.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "email tidak valid")
 		return
 	}
-
-	// Periksa apakah email sudah terdaftar
 	userExists, _ := utils.GetUserFromEmail(user.Email, db)
 	if user.Email == userExists.Email {
-		utils.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "Email sudah terdaftar")
+		utils.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "email sudah terdaftar")
 		return
 	}
-
-	// Validasi password
 	if strings.Contains(user.Password, " ") {
-		utils.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "Password tidak boleh mengandung spasi")
+		utils.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "password tidak boleh mengandung spasi")
 		return
 	}
 	if len(user.Password) < 8 {
-		utils.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "Password minimal 8 karakter")
+		utils.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "password minimal 8 karakter")
 		return
 	}
-	if user.Password != user.Confirmpassword {
-		utils.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "Password dan konfirmasi password tidak cocok")
-		return
-	}
-
-	// Membuat salt untuk password hashing
 	salt := make([]byte, 16)
 	_, err = rand.Read(salt)
 	if err != nil {
-		utils.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "Kesalahan server: gagal membuat salt")
+		utils.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : salt")
 		return
 	}
-
-	// Hash password menggunakan Argon2
 	hashedPassword := argon2.IDKey([]byte(user.Password), salt, 1, 64*1024, 4, 32)
-
-	// Mempersiapkan data pengguna untuk disimpan di database
 	userData := bson.M{
 		"namalengkap": user.NamaLengkap,
 		"email":       user.Email,
 		"password":    hex.EncodeToString(hashedPassword),
 		"salt":        hex.EncodeToString(salt),
 	}
-
-	// Menyimpan data pengguna di database
 	insertedID, err := utils.InsertOneDoc(db, col, userData)
 	if err != nil {
-		utils.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "Kesalahan server: gagal menyimpan data pengguna, "+err.Error())
+		utils.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : insert data, "+err.Error())
 		return
 	}
-
-	// Mengirim respon sukses
 	resp := map[string]any{
-		"message":    "Berhasil mendaftar",
+		"message":    "berhasil mendaftar",
 		"insertedID": insertedID,
 		"data": map[string]string{
 			"email": user.Email,
