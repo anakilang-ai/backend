59fff27b94bb5ca7290149d69bf854fdbe470ec2
Ratusukmakomala <ratukomala28@gmail.com>
2024-07-20 18:50:03 +0700

    buat helper

--- helper/helper.go
+++ helper/helper.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 )
 
+// ErrorResponse mengirimkan respon error dalam format JSON dengan status code yang sesuai
 func ErrorResponse(respw http.ResponseWriter, req *http.Request, statusCode int, err, msg string) {
 	resp := map[string]string{
 		"error":   err,
@@ -14,16 +15,24 @@ func ErrorResponse(respw http.ResponseWriter, req *http.Request, statusCode int,
 	WriteJSON(respw, statusCode, resp)
 }
 
+// WriteJSON menulis respon dalam format JSON dengan status code yang diberikan
 func WriteJSON(respw http.ResponseWriter, statusCode int, content any) {
 	respw.Header().Set("Content-Type", "application/json")
 	respw.WriteHeader(statusCode)
-	respw.Write([]byte(Jsonstr(content)))
+	jsonData, err := json.Marshal(content)
+	if err != nil {
+		http.Error(respw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	respw.Write(jsonData)
 }
 
+// Jsonstr mengubah struktur data ke dalam format string JSON
 func Jsonstr(strc any) string {
 	jsonData, err := json.Marshal(strc)
 	if err != nil {
-		log.Fatal(err)
+		log.Println("Error marshalling JSON:", err)
+		return ""
 	}
 	return string(jsonData)
 }
