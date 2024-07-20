12afdb9ece2a9058d2491bbd5aba9988b37983da
jxxzy <marjuniatiputri@gmail.com>
2024-07-20 18:28:45 +0700

    add function getEnv

--- modules/config.go
+++ modules/config.go
@@ -1,38 +1,12 @@
-package modul
+package modules
 
 import (
-	"log"
 	"os"
-
-	"github.com/joho/godotenv"
+	// "github.com/joho/godotenv"
 )
 
-// LoadEnv memuat variabel lingkungan dari file .env.
-func LoadEnv() {
-	err := godotenv.Load("../.env")
-	if err != nil {
-		log.Printf("Error loading .env file: %v", err)
-	}
-}
-
-// GetEnv mengambil nilai dari variabel lingkungan yang diberi nama oleh kunci.
-// Mengembalikan string kosong jika variabel tersebut tidak ada.
 func GetEnv(envName string) string {
+	// envFile, _ := godotenv.Read("../.env")
+	// return envFile[envName]
 	return os.Getenv(envName)
-}
-
-package main
-
-import (
-	"fmt"
-	"jalur_proyek_anda/modul"
-)
-
-func main() {
-	// Memuat variabel lingkungan dari file .env
-	modul.LoadEnv()
-
-	// Mengambil variabel lingkungan dibawah ini:
-	nilai := modul.GetEnv("YOUR_ENV_VARIABLE")
-	fmt.Println("Nilai dari YOUR_ENV_VARIABLE:", nilai)
 }
