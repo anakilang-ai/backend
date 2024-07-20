05a049bed9a9ca1cf7190c5554c8af0b23c64f19
jxxzy <marjuniatiputri@gmail.com>
2024-07-20 18:37:39 +0700

    add  handler routes.URL

--- routes/routes.go
+++ routes/routes.go
@@ -1,42 +1,15 @@
-package routes
+package main
 
 import (
+	"fmt"
 	"net/http"
 
-	controller "github.com/anakilang-ai/backend/controller"
-	"github.com/anakilang-ai/backend/helper"
-	"github.com/anakilang-ai/backend/modules"
+	"github.com/anakilang-ai/backend/routes"
 )
 
-func URL(w http.ResponseWriter, r *http.Request) {
-	if modules.SetAccessControlHeaders(w, r) {
-		return // If it's a preflight request, return early.
-	}
-
-	if modules.ErrorMongoconn != nil {
-		helper.ErrorResponse(w, r, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : database, "+modules.ErrorMongoconn.Error())
-		return
-	}
-
-	var method, path string = r.Method, r.URL.Path
-	switch {
-	case method == "GET" && path == "/":
-		Home(w, r)
-	case method == "POST" && path == "/signup":
-		controller.SignUp(modules.Mongoconn, "users", w, r)
-	case method == "POST" && path == "/login":
-		controller.LogIn(modules.Mongoconn, w, r, modules.GetEnv("PASETOPRIVATEKEY"))
-	case method == "POST" && path == "/chat":
-		controller.Chat(w, r, modules.GetEnv("TOKENMODEL"))
-	default:
-		helper.ErrorResponse(w, r, http.StatusNotFound, "Not Found", "The requested resource was not found")
-	}
-}
-
-func Home(respw http.ResponseWriter, req *http.Request) {
-	resp := map[string]string{
-		"github_repo": "https://github.com/anakilang-ai/backend",
-		"message":     "",
-	}
-	helper.WriteJSON(respw, http.StatusOK, resp)
+func main() {
+	http.HandleFunc("/", routes.URL)
+	port := ":8080"
+	fmt.Println("Server started at: http://localhost" + port)
+	http.ListenAndServe(port, nil)
 }
