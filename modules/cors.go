a9be01dd8aee3134bcf46e682f915a444641ff9f
Ratusukmakomala <ratukomala28@gmail.com>
2024-07-20 19:17:32 +0700

    Mengatur header CORS

--- modules/cors.go
+++ modules/cors.go
@@ -5,14 +5,14 @@ import (
 )
 
 // Daftar origins yang diizinkan
-var allowedOrigins = []string{
+var Origins = []string{
 	"http://localhost:8080",
 	"https://anakilang-ai.github.io/",
 }
 
-// isAllowedOrigin memeriksa apakah origin diizinkan
+// Fungsi untuk memeriksa apakah origin diizinkan
 func isAllowedOrigin(origin string) bool {
-	for _, o := range allowedOrigins {
+	for _, o := range Origins {
 		if o == origin {
 			return true
 		}
@@ -20,31 +20,26 @@ func isAllowedOrigin(origin string) bool {
 	return false
 }
 
-// SetAccessControlHeaders mengatur header CORS
-func SetAccessControlHeaders(w http.ResponseWriter, r *http.Request) {
+// Fungsi untuk mengatur header CORS
+func SetAccessControlHeaders(w http.ResponseWriter, r *http.Request) bool {
 	origin := r.Header.Get("Origin")
 
 	if isAllowedOrigin(origin) {
-		w.Header().Set("Access-Control-Allow-Credentials", "true")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Login")
-		w.Header().Set("Access-Control-Allow-Methods", "POST,GET,DELETE,PUT,OPTIONS")
-		w.Header().Set("Access-Control-Allow-Origin", origin)
-		w.Header().Set("Access-Control-Max-Age", "3600")
-
+		// Set CORS headers for the preflight request
 		if r.Method == http.MethodOptions {
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Login")
+			w.Header().Set("Access-Control-Allow-Methods", "POST,GET,DELETE,PUT")
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Max-Age", "3600")
 			w.WriteHeader(http.StatusNoContent)
-			return
+			return true
 		}
+		// Set CORS headers for the main request.
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Origin", origin)
+		return false
 	}
-}
 
-// Middleware CORS untuk HTTP handler
-func CORSMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		SetAccessControlHeaders(w, r)
-		if r.Method == http.MethodOptions {
-			return
-		}
-		next.ServeHTTP(w, r)
-	})
+	return false
 }
