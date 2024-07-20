0712bbe463afba663013d3388434e1dff8e349a7
jxxzy <marjuniatiputri@gmail.com>
2024-07-20 18:30:09 +0700

    set headers Cors

--- modules/cors.go
+++ modules/cors.go
@@ -28,8 +28,8 @@ func SetAccessControlHeaders(w http.ResponseWriter, r *http.Request) bool {
 		// Set CORS headers for the preflight request
 		if r.Method == http.MethodOptions {
 			w.Header().Set("Access-Control-Allow-Credentials", "true")
-			w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Login")
-			w.Header().Set("Access-Control-Allow-Methods", "POST,GET,DELETE,PUT")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Login")
+			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS")
 			w.Header().Set("Access-Control-Allow-Origin", origin)
 			w.Header().Set("Access-Control-Max-Age", "3600")
 			w.WriteHeader(http.StatusNoContent)
@@ -38,7 +38,6 @@ func SetAccessControlHeaders(w http.ResponseWriter, r *http.Request) bool {
 		// Set CORS headers for the main request.
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Origin", origin)
-		return false
 	}
 
 	return false
