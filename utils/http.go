e686c425bdfeb17e5469d15e7784663b81c0a068
jxxzy <marjuniatiputri@gmail.com>
2024-07-21 18:16:43 +0700

    edit package utils

--- utils/http.go
+++ utils/http.go
@@ -6,24 +6,31 @@ import (
 	"net/http"
 )
 
+// ErrorResponse menangani respons error JSON dengan pesan yang sesuai.
 func ErrorResponse(respw http.ResponseWriter, req *http.Request, statusCode int, err, msg string) {
 	resp := map[string]string{
 		"error":   err,
 		"message": msg,
 	}
-	WriteJSON(respw, statusCode, resp)
+	writeJSON(respw, statusCode, resp)
 }
 
-func WriteJSON(respw http.ResponseWriter, statusCode int, content any) {
+// WriteJSON menulis response JSON ke ResponseWriter dengan status code yang diberikan.
+func WriteJSON(respw http.ResponseWriter, statusCode int, content interface{}) {
 	respw.Header().Set("Content-Type", "application/json")
 	respw.WriteHeader(statusCode)
-	respw.Write([]byte(Jsonstr(content)))
+	err := json.NewEncoder(respw).Encode(content)
+	if err != nil {
+		log.Println("Error encoding JSON:", err)
+	}
 }
 
-func Jsonstr(strc any) string {
-	jsonData, err := json.Marshal(strc)
+// jsonStr mengembalikan representasi JSON dari objek sebagai string.
+func jsonStr(obj interface{}) string {
+	jsonData, err := json.Marshal(obj)
 	if err != nil {
-		log.Fatal(err)
+		log.Println("Error marshaling JSON:", err)
+		return ""
 	}
 	return string(jsonData)
 }
