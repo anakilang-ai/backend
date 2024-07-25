2dda0c321187160c97e3519cab24f636c301cf1f
Ratusukmakomala <ratukomala28@gmail.com>
2024-07-25 22:16:59 +0700

    membuat config

--- modules/config.go
+++ modules/config.go
@@ -1,12 +1,26 @@
-package modules
+// config.go
+package config
 
 import (
+	"log"
 	"os"
-	// "github.com/joho/godotenv"
+
+	"github.com/joho/godotenv"
 )
 
-func GetEnv(envName string) string {
-	// envFile, _ := godotenv.Read("../.env")
-	// return envFile[envName]
-	return os.Getenv(envName)
+// LoadEnv loads environment variables from a .env file if it exists.
+func LoadEnv() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found or error loading .env file")
+	}
+}
+
+// GetEnv retrieves the value of the environment variable named by the key.
+// It returns the default value if the key does not exist.
+func GetEnv(key string, defaultValue ...string) string {
+	value := os.Getenv(key)
+	if value == "" && len(defaultValue) > 0 {
+		return defaultValue[0]
+	}
+	return value
 }
