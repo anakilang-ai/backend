c8809c50cd002be94d9f730623c793107822d6d9
Ratusukmakomala <ratukomala28@gmail.com>
2024-07-25 22:27:32 +0700

    membuat db

--- modules/db.go
+++ modules/db.go
@@ -1,26 +1,14 @@
-// db.go
 package modules
 
 import (
-	"log"
-
-	"github.com/anakilang-ai/backend/config" // Import config package for environment variables
 	"github.com/anakilang-ai/backend/utils"
 )
 
-// Initialize MongoDB connection details
-var (
-	MongoString = config.GetEnv("MONGOSTRING")
-	mongoInfo   = utils.DBInfo{
-		DBString: MongoString,
-		DBName:   "ailang",
-	}
-	MongoConn, MongoConnErr = utils.MongoConnect(mongoInfo)
-)
+var MongoString string = GetEnv("MONGOSTRING")
 
-func init() {
-	if MongoConnErr != nil {
-		log.Fatalf("Failed to connect to MongoDB: %v", MongoConnErr)
-	}
-	log.Println("Successfully connected to MongoDB")
+var mongoinfo = utils.DBInfo{
+	DBString: MongoString,
+	DBName:   "ailang",
 }
+
+var Mongoconn, ErrorMongoconn = utils.MongoConnect(mongoinfo)
