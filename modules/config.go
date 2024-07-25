1d4c9d8623c85650d1e531f08b317a51db5dc9a4
Ratusukmakomala <ratukomala28@gmail.com>
2024-07-25 22:25:26 +0700

    mengubah config

--- modules/config.go
+++ modules/config.go
@@ -1,32 +1,12 @@
-// config.go
-package config
+package modules
 
 import (
-	"log"
 	"os"
-
-	"github.com/joho/godotenv"
+	// "github.com/joho/godotenv"
 )
 
-// LoadEnv loads environment variables from a .env file if it exists.
-// It logs an error message if the .env file cannot be found or loaded.
-func LoadEnv() {
-	if err := godotenv.Load(); err != nil {
-		if os.IsNotExist(err) {
-			log.Println(".env file does not exist")
-		} else {
-			log.Printf("Error loading .env file: %v", err)
-		}
-	}
-}
-
-// GetEnv retrieves the value of the environment variable named by the key.
-// It returns the default value if the key does not exist or is empty.
-// If no default value is provided and the key does not exist, it returns an empty string.
-func GetEnv(key string, defaultValue ...string) string {
-	value := os.Getenv(key)
-	if value == "" && len(defaultValue) > 0 {
-		return defaultValue[0]
-	}
-	return value
+func GetEnv(envName string) string {
+	// envFile, _ := godotenv.Read("../.env")
+	// return envFile[envName]
+	return os.Getenv(envName)
 }
--- modules/db.go
+++ modules/db.go
@@ -4,22 +4,23 @@ package modules
 import (
 	"log"
 
+	"github.com/anakilang-ai/backend/config" // Import config package for environment variables
 	"github.com/anakilang-ai/backend/utils"
 )
 
-var MongoString string = GetEnv("MONGOSTRING")
-
-var mongoinfo = utils.DBInfo{
-	DBString: MongoString,
-	DBName:   "ailang",
-}
-
-var Mongoconn, ErrorMongoconn = utils.MongoConnect(mongoinfo)
+// Initialize MongoDB connection details
+var (
+	MongoString = config.GetEnv("MONGOSTRING")
+	mongoInfo   = utils.DBInfo{
+		DBString: MongoString,
+		DBName:   "ailang",
+	}
+	MongoConn, MongoConnErr = utils.MongoConnect(mongoInfo)
+)
 
 func init() {
-	if ErrorMongoconn != nil {
-		log.Fatalf("Failed to connect to MongoDB: %v", ErrorMongoconn)
-	} else {
-		log.Println("Successfully connected to MongoDB")
+	if MongoConnErr != nil {
+		log.Fatalf("Failed to connect to MongoDB: %v", MongoConnErr)
 	}
+	log.Println("Successfully connected to MongoDB")
 }
