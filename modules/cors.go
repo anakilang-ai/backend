ade84dd47484d9e5e1f9c0c18b00cbafa307454d
jxxzy <marjuniatiputri@gmail.com>
2024-07-29 19:56:51 +0700

    Consistent Header

--- modules/cors.go
+++ modules/cors.go
@@ -4,13 +4,13 @@ import (
 	"net/http"
 )
 
-// List of allowed origins
+// Daftar origins yang diizinkan
 var Origins = []string{
 	"http://localhost:8080",
 	"https://anakilang-ai.github.io/",
 }
 
-// Checks if the origin is allowed
+// Fungsi untuk memeriksa apakah origin diizinkan
 func isAllowedOrigin(origin string) bool {
 	for _, o := range Origins {
 		if o == origin {
@@ -20,26 +20,25 @@ func isAllowedOrigin(origin string) bool {
 	return false
 }
 
-// Sets CORS headers for the response
+// Fungsi untuk mengatur header CORS
 func SetAccessControlHeaders(w http.ResponseWriter, r *http.Request) bool {
 	origin := r.Header.Get("Origin")
 
-	if !isAllowedOrigin(origin) {
-		// If the origin is not allowed, do not set CORS headers
+	if isAllowedOrigin(origin) {
+		// Set CORS headers for the preflight request
+		if r.Method == http.MethodOptions {
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Login")
+			w.Header().Set("Access-Control-Allow-Methods", "POST,GET,DELETE,PUT")
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Max-Age", "3600")
+			w.WriteHeader(http.StatusNoContent)
+			return true
+		}
+		// Set CORS headers for the main request.
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Origin", origin)
 		return false
-	}
-
-	// Set CORS headers
-	w.Header().Set("Access-Control-Allow-Credentials", "true")
-	w.Header().Set("Access-Control-Allow-Origin", origin)
-
-	// Handle preflight requests
-	if r.Method == http.MethodOptions {
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Login")
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT")
-		w.Header().Set("Access-Control-Max-Age", "3600")
-		w.WriteHeader(http.StatusNoContent)
-		return true
 	}
 
 	return false
