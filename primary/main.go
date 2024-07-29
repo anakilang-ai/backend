3e696c626aa50eb021f3e4d84704cc6bb4fec791
jxxzy <marjuniatiputri@gmail.com>
2024-07-29 19:58:51 +0700

    add handlefunction

--- primary/main.go
+++ primary/main.go
@@ -1,51 +1,20 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
 	"net/http"
-	"os"
-	"os/signal"
-	"time"
 
 	"github.com/anakilang-ai/backend/routes"
 )
 
 func main() {
-	// Set up a new server mux and attach the URL route
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", routes.URL)
+	http.HandleFunc("/", routes.URL)
+	port := ":8080"
+	fmt.Printf("Server started at: http://localhost%s\n", port)
 
-	// Define the server with a specific address and handler
-	server := &http.Server{
-		Addr:    ":8080",
-		Handler: mux,
+	// Menjalankan server dan menangani kesalahan jika terjadi
+	if err := http.ListenAndServe(port, nil); err != nil {
+		log.Fatalf("Server gagal untuk memulai: %v", err)
 	}
-
-	// Run the server in a separate goroutine to allow for graceful shutdown
-	go func() {
-		fmt.Printf("Server started at: http://localhost%s\n", server.Addr)
-		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("Server failed to start: %v", err)
-		}
-	}()
-
-	// Set up channel to listen for OS interrupt signals
-	stop := make(chan os.Signal, 1)
-	signal.Notify(stop, os.Interrupt)
-
-	// Block until an interrupt signal is received
-	<-stop
-
-	// Create a context with a timeout to allow for graceful shutdown
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	// Attempt to gracefully shut down the server
-	if err := server.Shutdown(ctx); err != nil {
-		log.Fatalf("Server shutdown failed: %v", err)
-	}
-
-	fmt.Println("Server gracefully stopped")
 }
