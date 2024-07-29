3345a02da5e815745b8bfc4910785f29f42998ce
Ratusukmakomala <ratukomala28@gmail.com>
2024-07-29 20:40:42 +0700

    Kirim respons sukses

--- controller/Chat.go
+++ controller/Chat.go
@@ -7,7 +7,7 @@ import (
 	"net/http"
 	"strings"
 
-	model "github.com/anakilang-ai/backend/models"
+	"github.com/anakilang-ai/backend/models"
 	"github.com/anakilang-ai/backend/utils"
 	"github.com/badoux/checkmail"
 	"go.mongodb.org/mongo-driver/bson"
@@ -15,15 +15,18 @@ import (
 	"golang.org/x/crypto/argon2"
 )
 
+// SignUp menangani proses pendaftaran pengguna baru
 func SignUp(db *mongo.Database, col string, respw http.ResponseWriter, req *http.Request) {
-	var user model.User
+	var user models.User
 
+	// Decode request body menjadi struct User
 	err := json.NewDecoder(req.Body).Decode(&user)
 	if err != nil {
 		utils.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "error parsing request body "+err.Error())
 		return
 	}
 
+	// Validasi input
 	if user.NamaLengkap == "" || user.Email == "" || user.Password == "" || user.Confirmpassword == "" {
 		utils.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "mohon untuk melengkapi data")
 		return
@@ -32,11 +35,15 @@ func SignUp(db *mongo.Database, col string, respw http.ResponseWriter, req *http
 		utils.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "email tidak valid")
 		return
 	}
+
+	// Periksa apakah email sudah terdaftar
 	userExists, _ := utils.GetUserFromEmail(user.Email, db)
 	if user.Email == userExists.Email {
 		utils.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "email sudah terdaftar")
 		return
 	}
+
+	// Validasi password
 	if strings.Contains(user.Password, " ") {
 		utils.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "password tidak boleh mengandung spasi")
 		return
@@ -45,6 +52,8 @@ func SignUp(db *mongo.Database, col string, respw http.ResponseWriter, req *http
 		utils.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "password minimal 8 karakter")
 		return
 	}
+
+	// Generate salt dan hash password
 	salt := make([]byte, 16)
 	_, err = rand.Read(salt)
 	if err != nil {
@@ -58,11 +67,15 @@ func SignUp(db *mongo.Database, col string, respw http.ResponseWriter, req *http
 		"password":    hex.EncodeToString(hashedPassword),
 		"salt":        hex.EncodeToString(salt),
 	}
+
+	// Masukkan data pengguna ke database
 	insertedID, err := utils.InsertOneDoc(db, col, userData)
 	if err != nil {
 		utils.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : insert data, "+err.Error())
 		return
 	}
+
+	// Kirim respons sukses
 	resp := map[string]any{
 		"message":    "berhasil mendaftar",
 		"insertedID": insertedID,
