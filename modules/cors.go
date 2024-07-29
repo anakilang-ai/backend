d3a2a4ff4174290f22f0f1629469c52d801faf22
jxxzy <marjuniatiputri@gmail.com>
2024-07-29 19:56:23 +0700

    allow additional headers

--- modules/cors.go
+++ modules/cors.go
@@ -4,13 +4,13 @@ import (
 	"net/http"
 )
 
-// Daftar origins yang diizinkan
+// List of allowed origins
 var Origins = []string{
 	"http://localhost:8080",
 	"https://anakilang-ai.github.io/",
 }
 
-// Fungsi untuk memeriksa apakah origin diizinkan
+// Checks if the origin is allowed
 func isAllowedOrigin(origin string) bool {
 	for _, o := range Origins {
 		if o == origin {
@@ -20,25 +20,26 @@ func isAllowedOrigin(origin string) bool {
 	return false
 }
 
-// Fungsi untuk mengatur header CORS
+// Sets CORS headers for the response
 func SetAccessControlHeaders(w http.ResponseWriter, r *http.Request) bool {
 	origin := r.Header.Get("Origin")
 
-	if isAllowedOrigin(origin) {
-		// Set CORS headers for the preflight request
-		if r.Method == http.MethodOptions {
-			w.Header().Set("Access-Control-Allow-Credentials", "true")
-			w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Login")
-			w.Header().Set("Access-Control-Allow-Methods", "POST,GET,DELETE,PUT")
-			w.Header().Set("Access-Control-Allow-Origin", origin)
-			w.Header().Set("Access-Control-Max-Age", "3600")
-			w.WriteHeader(http.StatusNoContent)
-			return true
-		}
-		// Set CORS headers for the main request.
-		w.Header().Set("Access-Control-Allow-Credentials", "true")
-		w.Header().Set("Access-Control-Allow-Origin", origin)
+	if !isAllowedOrigin(origin) {
+		// If the origin is not allowed, do not set CORS headers
 		return false
+	}
+
+	// Set CORS headers
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+	w.Header().Set("Access-Control-Allow-Origin", origin)
+
+	// Handle preflight requests
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Login")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+		return true
 	}
 
 	return false
