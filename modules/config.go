90674e0780df608bc2d48063ccb517ab4c769271
Ratusukmakomala <ratukomala28@gmail.com>
2024-07-31 21:13:41 +0700

    cetakperingatan jk variabellingkungan tidak diatur

--- modules/config.go
+++ modules/config.go
@@ -1,15 +1,17 @@
 package modules
 
 import (
-	"os"
 	"log"
+	"os"
 )
 
 // GetEnv retrieves the value of the environment variable named by envName.
 // It returns an empty string if the environment variable is not set.
 func GetEnv(envName string) string {
+	// Mengambil nilai variabel lingkungan
 	value := os.Getenv(envName)
 	if value == "" {
+		// Mencetak peringatan jika variabel lingkungan tidak diatur
 		log.Printf("Warning: Environment variable %s is not set.", envName)
 	}
 	return value
