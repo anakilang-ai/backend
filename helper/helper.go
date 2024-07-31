94bfd0d676d484a637fd55a0d42f537c682d4dab
Ratusukmakomala <ratukomala28@gmail.com>
2024-07-31 20:22:54 +0700

    update helper

--- helper/helper.go
+++ helper/helper.go
@@ -6,7 +6,6 @@ import (
 	"net/http"
 )
 
-// ErrorResponse mengirimkan respons JSON berisi pesan kesalahan
 func ErrorResponse(respw http.ResponseWriter, req *http.Request, statusCode int, err, msg string) {
 	resp := map[string]string{
 		"error":   err,
@@ -15,14 +14,12 @@ func ErrorResponse(respw http.ResponseWriter, req *http.Request, statusCode int,
 	WriteJSON(respw, statusCode, resp)
 }
 
-// WriteJSON mengirimkan respons JSON dengan konten yang diberikan
 func WriteJSON(respw http.ResponseWriter, statusCode int, content any) {
 	respw.Header().Set("Content-Type", "application/json")
 	respw.WriteHeader(statusCode)
 	respw.Write([]byte(Jsonstr(content)))
 }
 
-// Jsonstr mengubah struktur data menjadi string JSON
 func Jsonstr(strc any) string {
 	jsonData, err := json.Marshal(strc)
 	if err != nil {
