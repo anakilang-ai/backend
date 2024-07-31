41afd78caf773ca8213caee7638f81810265975b
Ratusukmakomala <ratukomala28@gmail.com>
2024-07-31 21:17:10 +0700

    update example-cl

--- helper/example-cl.go
+++ helper/example-cl.go
@@ -9,7 +9,6 @@ import (
 )
 
 func main() {
-	// Mencoba untuk terhubung ke server TCP di localhost pada port 8081
 	koneksi, err := net.Dial("tcp", "localhost:8081")
 	if err != nil {
 		fmt.Println("Kesalahan saat menghubungkan ke server:", err)
@@ -18,34 +17,18 @@ func main() {
 	defer koneksi.Close()
 
 	fmt.Println("Terhubung ke server")
-
-	// Goroutine untuk menerima pesan dari server
 	go func() {
 		for {
-			// Membaca pesan dari server
-			pesan, err := bufio.NewReader(koneksi).ReadString('\n')
-			if err != nil {
-				fmt.Println("Kesalahan saat membaca pesan dari server:", err)
-				return
-			}
-			// Menampilkan pesan yang diterima dari server
-			fmt.Print("Pesan dari server: ", pesan)
+			pesan, _ := bufio.NewReader(koneksi).ReadString('\n')
+			fmt.Print("Pesan dari server:", string(pesan))
 		}
 	}()
 
-	// Loop untuk mengirim pesan ke server
 	for {
 		pembaca := bufio.NewReader(os.Stdin)
 		fmt.Print("Masukkan pesan: ")
-		// Membaca input dari pengguna
-		pesan, err := pembaca.ReadString('\n')
-		if err != nil {
-			fmt.Println("Kesalahan saat membaca input:", err)
-			continue
-		}
-		// Mengirim pesan ke server
+		pesan, _ := pembaca.ReadString('\n')
 		fmt.Fprintf(koneksi, pesan+"\n")
-		// Jika pesan adalah "KELUAR", menutup koneksi dan keluar
 		if strings.TrimSpace(pesan) == "KELUAR" {
 			fmt.Println("Menutup koneksi")
 			return
