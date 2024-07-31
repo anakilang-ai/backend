99bf79476b258f3d5b563b176dd9d2e09c93adf6
GilarYa <gilarwahibul@gmail.com>
2024-07-31 17:02:52 +0700

    replace

--- helper/example-sv.go
+++ helper/example-sv.go
@@ -2,44 +2,7 @@ package main
 
 import (
     "bufio"
-    "fmt"package main
-
-	import (
-		"bufio"
-		"fmt"
-		"net"
-		"os"
-		"strings"
-	)
-	
-	func main() {
-		koneksi, err := net.Dial("tcp", "localhost:8081")
-		if err != nil {
-			fmt.Println("Kesalahan saat menghubungkan ke server:", err)
-			os.Exit(1)
-		}
-		defer koneksi.Close()
-	
-		fmt.Println("Terhubung ke server")
-		go func() {
-			for {
-				pesan, _ := bufio.NewReader(koneksi).ReadString('\n')
-				fmt.Print("Pesan dari server:", string(pesan))
-			}
-		}()
-	
-		for {
-			pembaca := bufio.NewReader(os.Stdin)
-			fmt.Print("Masukkan pesan: ")
-			pesan, _ := pembaca.ReadString('\n')
-			fmt.Fprintf(koneksi, pesan+"\n")
-			if strings.TrimSpace(pesan) == "KELUAR" {
-				fmt.Println("Menutup koneksi")
-				return
-			}
-		}
-	}
-	
+    "fmt"
     "net"
     "os"
     "strings"
