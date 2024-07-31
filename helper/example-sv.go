9ac1e142f17e2a418796f286d5594c48bfe6e89d
Ratusukmakomala <ratukomala28@gmail.com>
2024-07-31 19:59:00 +0700

    update example-sv.go

--- helper/example-sv.go
+++ helper/example-sv.go
@@ -8,33 +8,23 @@ import (
 	"strings"
 )
 
-// Fungsi untuk menangani koneksi dari klien
 func tanganiKoneksi(koneksi net.Conn) {
 	fmt.Println("Klien terhubung:", koneksi.RemoteAddr().String())
 	defer koneksi.Close()
 
 	for {
-		pesan, err := bufio.NewReader(koneksi).ReadString('\n')
-		if err != nil {
-			fmt.Println("Kesalahan saat membaca pesan dari klien:", err)
-			return
-		}
-
-		// Memeriksa apakah pesan adalah "KELUAR" untuk mengakhiri koneksi
+		pesan, _ := bufio.NewReader(koneksi).ReadString('\n')
 		if strings.TrimSpace(pesan) == "KELUAR" {
 			fmt.Println("Klien terputus:", koneksi.RemoteAddr().String())
 			return
 		}
-
-		fmt.Print("Pesan diterima:", pesan)
+		fmt.Print("Pesan diterima:", string(pesan))
 		koneksi.Write([]byte(pesan))
 	}
 }
 
 func main() {
 	fmt.Println("Memulai server...")
-
-	// Membuat pendengar TCP di localhost pada port 8081
 	pendengar, err := net.Listen("tcp", "localhost:8081")
 	if err != nil {
 		fmt.Println("Kesalahan saat membuat server:", err)
@@ -43,16 +33,12 @@ func main() {
 	defer pendengar.Close()
 
 	fmt.Println("Server berjalan di localhost:8081")
-
 	for {
-		// Menerima koneksi dari klien
 		koneksi, err := pendengar.Accept()
 		if err != nil {
 			fmt.Println("Kesalahan saat menerima koneksi:", err)
 			continue
 		}
-
-		// Menangani koneksi dalam goroutine terpisah
 		go tanganiKoneksi(koneksi)
 	}
 }
