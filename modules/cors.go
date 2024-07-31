d426d461357d1c26bab4266d4827e1945c78fcb0
Ratusukmakomala <ratukomala28@gmail.com>
2024-07-31 20:28:14 +0700

    membuat cors

--- modules/cors.go
+++ modules/cors.go
@@ -5,15 +5,15 @@ import (
 )
 
 // Daftar origins yang diizinkan
-var AllowedOrigins = []string{
+var Origins = []string{
 	"http://localhost:8080",
 	"https://anakilang-ai.github.io/",
 }
 
 // Fungsi untuk memeriksa apakah origin diizinkan
 func isAllowedOrigin(origin string) bool {
-	for _, allowed := range AllowedOrigins {
-		if allowed == origin {
+	for _, o := range Origins {
+		if o == origin {
 			return true
 		}
 	}
@@ -24,19 +24,18 @@ func isAllowedOrigin(origin string) bool {
 func SetAccessControlHeaders(w http.ResponseWriter, r *http.Request) bool {
 	origin := r.Header.Get("Origin")
 
-	// Periksa jika origin diizinkan
 	if isAllowedOrigin(origin) {
-		// Set header untuk permintaan preflight
+		// Set CORS headers for the preflight request
 		if r.Method == http.MethodOptions {
 			w.Header().Set("Access-Control-Allow-Credentials", "true")
-			w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization") // Mengganti 'Login' dengan 'Authorization' untuk kepatuhan standar
-			w.Header().Set("Access-Control-Allow-Methods", "POST,GET,DELETE,PUT,OPTIONS")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Login")
+			w.Header().Set("Access-Control-Allow-Methods", "POST,GET,DELETE,PUT")
 			w.Header().Set("Access-Control-Allow-Origin", origin)
 			w.Header().Set("Access-Control-Max-Age", "3600")
 			w.WriteHeader(http.StatusNoContent)
 			return true
 		}
-		// Set header untuk permintaan utama
+		// Set CORS headers for the main request.
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Origin", origin)
 		return false
