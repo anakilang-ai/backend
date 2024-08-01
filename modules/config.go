2fcc9201077f15aca03363345decda409c2e2599
Ratusukmakomala <ratukomala28@gmail.com>
2024-08-01 15:26:23 +0700

    Default Value Mechanism

--- modules/config.go
+++ modules/config.go
@@ -1,27 +1,35 @@
 package modules
 
 import (
-	"github.com/joho/godotenv"
 	"log"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
-// LoadEnv loads environment variables from a .env file if it exists.
-// It returns an error if it fails to load the file or parse it.
+// LoadEnv loads environment variables from a .env file.
+// It returns an error if the file cannot be loaded or if there's an issue reading it.
 func LoadEnv(filePath string) error {
-	err := godotenv.Load(filePath)
-	if err != nil {
+	if _, err := os.Stat(filePath); err == nil {
+		if err := godotenv.Load(filePath); err != nil {
+			return err
+		}
+	} else if !os.IsNotExist(err) {
 		return err
 	}
 	return nil
 }
 
 // GetEnv retrieves the value of the environment variable named by envName.
-// It returns the value of the environment variable or an empty string if it is not set.
-func GetEnv(envName string) string {
+// If the environment variable is not set, it returns a default value if provided.
+func GetEnv(envName string, defaultValue ...string) string {
 	value, exists := os.LookupEnv(envName)
 	if !exists {
+		if len(defaultValue) > 0 {
+			return defaultValue[0]
+		}
 		log.Printf("Warning: Environment variable %s not set", envName)
+		return ""
 	}
 	return value
 }
