9f48fd30001efa525cb61d487b0230240013eab9
Ratusukmakomala <ratukomala28@gmail.com>
2024-08-01 15:33:47 +0700

    config

--- modules/config.go
+++ modules/config.go
@@ -1,35 +1,12 @@
 package modules
 
 import (
-	"log"
 	"os"
-
-	"github.com/joho/godotenv"
+	// "github.com/joho/godotenv"
 )
 
-// LoadEnv loads environment variables from a .env file.
-// It returns an error if the file cannot be loaded or if there's an issue reading it.
-func LoadEnv(filePath string) error {
-	if _, err := os.Stat(filePath); err == nil {
-		if err := godotenv.Load(filePath); err != nil {
-			return err
-		}
-	} else if !os.IsNotExist(err) {
-		return err
-	}
-	return nil
-}
-
-// GetEnv retrieves the value of the environment variable named by envName.
-// If the environment variable is not set, it returns a default value if provided.
-func GetEnv(envName string, defaultValue ...string) string {
-	value, exists := os.LookupEnv(envName)
-	if !exists {
-		if len(defaultValue) > 0 {
-			return defaultValue[0]
-		}
-		log.Printf("Warning: Environment variable %s not set", envName)
-		return ""
-	}
-	return value
+func GetEnv(envName string) string {
+	// envFile, _ := godotenv.Read("../.env")
+	// return envFile[envName]
+	return os.Getenv(envName)
 }
