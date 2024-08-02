a449714e0208b305030e9b42a56a232f29e37d06
jxxzy <marjuniatiputri@gmail.com>
2024-08-02 16:00:25 +0700

    Improved comments and added an example usage section

--- modules/config.go
+++ modules/config.go
@@ -1,30 +1,32 @@
 package modules
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/joho/godotenv"
 )
 
 // LoadEnv loads environment variables from a .env file if it exists.
-// This function is optional and can be used for local development.
+// This function is useful for local development and testing.
+// Returns an error if loading the .env file fails.
 func LoadEnv(filePath string) error {
 	if _, err := os.Stat(filePath); !os.IsNotExist(err) {
 		if err := godotenv.Load(filePath); err != nil {
-			return err
+			return fmt.Errorf("error loading .env file: %w", err)
 		}
 	}
 	return nil
 }
 
 // GetEnv retrieves the value of the environment variable named by envName.
-// It returns an empty string if the variable is not set.
+// Returns an empty string if the variable is not set.
 func GetEnv(envName string) string {
 	return os.Getenv(envName)
 }
 
 // GetEnvOrDefault retrieves the value of the environment variable named by envName.
-// If the variable is not set, it returns the provided default value.
+// If the variable is not set, returns the provided default value.
 func GetEnvOrDefault(envName, defaultValue string) string {
 	value := os.Getenv(envName)
 	if value == "" {
