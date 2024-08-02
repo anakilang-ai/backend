0eb7a019f7de6492359203c42e73f8f6f0d646c5
jxxzy <marjuniatiputri@gmail.com>
2024-08-02 15:58:30 +0700

    Improved messages and handling

--- utils/mongo.go
+++ utils/mongo.go
@@ -19,67 +19,81 @@ type DBInfo struct {
 }
 
 // MongoConnect establishes a connection to MongoDB and returns a database instance.
-func MongoConnect(mconn DBInfo) (db *mongo.Database, err error) {
+func MongoConnect(mconn DBInfo) (*mongo.Database, error) {
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(mconn.DBString))
 	if err != nil {
-		return nil, fmt.Errorf("failed to connect to MongoDB: %v", err)
+		return nil, fmt.Errorf("failed to connect to MongoDB: %w", err)
 	}
+
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to ping MongoDB: %w", err)
+	}
+
 	return client.Database(mconn.DBName), nil
 }
 
 // InsertOneDoc inserts a single document into the specified collection and returns the inserted ID.
-func InsertOneDoc(db *mongo.Database, col string, doc any) (insertedID primitive.ObjectID, err error) {
+func InsertOneDoc(db *mongo.Database, col string, doc interface{}) (primitive.ObjectID, error) {
 	result, err := db.Collection(col).InsertOne(context.Background(), doc)
 	if err != nil {
-		return primitive.NilObjectID, fmt.Errorf("failed to insert document: %v", err)
+		return primitive.NilObjectID, fmt.Errorf("failed to insert document: %w", err)
 	}
+
 	id, ok := result.InsertedID.(primitive.ObjectID)
 	if !ok {
 		return primitive.NilObjectID, errors.New("inserted ID is not of type primitive.ObjectID")
 	}
+
 	return id, nil
 }
 
 // GetUserFromEmail retrieves a user document by email.
-func GetUserFromEmail(email string, db *mongo.Database) (doc models.User, err error) {
+func GetUserFromEmail(email string, db *mongo.Database) (models.User, error) {
 	collection := db.Collection("users")
 	filter := bson.M{"email": email}
-	err = collection.FindOne(context.TODO(), filter).Decode(&doc)
+	var doc models.User
+	err := collection.FindOne(context.TODO(), filter).Decode(&doc)
 	if err != nil {
 		if err == mongo.ErrNoDocuments {
-			return doc, fmt.Errorf("email not found: %v", err)
+			return doc, fmt.Errorf("email not found: %w", err)
 		}
-		return doc, fmt.Errorf("server error while retrieving user: %v", err)
+		return doc, fmt.Errorf("server error while retrieving user: %w", err)
 	}
+
 	return doc, nil
 }
 
 // GetAllDocs retrieves all documents from a specified collection that match the given filter.
-func GetAllDocs[T any](db *mongo.Database, col string, filter bson.M) (docs T, err error) {
+func GetAllDocs[T any](db *mongo.Database, col string, filter bson.M) ([]T, error) {
 	ctx := context.TODO()
 	collection := db.Collection(col)
 	cursor, err := collection.Find(ctx, filter)
 	if err != nil {
-		return docs, fmt.Errorf("failed to execute find query: %v", err)
+		return nil, fmt.Errorf("failed to execute find query: %w", err)
 	}
 	defer cursor.Close(ctx)
-	err = cursor.All(ctx, &docs)
-	if err != nil {
-		return docs, fmt.Errorf("failed to decode documents: %v", err)
+
+	var docs []T
+	if err = cursor.All(ctx, &docs); err != nil {
+		return nil, fmt.Errorf("failed to decode documents: %w", err)
 	}
+
 	return docs, nil
 }
 
 // GetUserFromID retrieves a user document by its ID.
-func GetUserFromID(_id primitive.ObjectID, db *mongo.Database) (doc models.User, err error) {
+func GetUserFromID(_id primitive.ObjectID, db *mongo.Database) (models.User, error) {
 	collection := db.Collection("users")
 	filter := bson.M{"_id": _id}
-	err = collection.FindOne(context.TODO(), filter).Decode(&doc)
+	var doc models.User
+	err := collection.FindOne(context.TODO(), filter).Decode(&doc)
 	if err != nil {
 		if errors.Is(err, mongo.ErrNoDocuments) {
-			return doc, fmt.Errorf("no data found for ID %s: %v", _id.Hex(), err)
+			return doc, fmt.Errorf("no data found for ID %s: %w", _id.Hex(), err)
 		}
-		return doc, fmt.Errorf("error retrieving data for ID %s: %v", _id.Hex(), err)
+		return doc, fmt.Errorf("error retrieving data for ID %s: %w", _id.Hex(), err)
 	}
+
 	return doc, nil
 }
